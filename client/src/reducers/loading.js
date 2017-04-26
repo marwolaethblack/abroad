de533e7889904861b88ba6b10a09ca78a532777c
Jakub Kozák <Jakub Kozk>
2017-04-26 21:20:56 +0200

    loading changed

--- client/src/reducers/loading.js
+++ client/src/reducers/loading.js
@@ -5,17 +5,21 @@ export const loading = (state=false, action) => {
     switch (action.type) {
         case AT.FETCH_POSTS:
         case AT.FETCH_SINGLE_POST:
-        case AT.ADDING_POST:
-        case AT.FETCH_USER: {
-            return true;
+        case AT.ADDING_POST: {
+            return {...state, posts: true};
         }
         case AT.FETCH_SINGLE_POST_DONE:
         case AT.FETCH_POSTS_DONE:
         case AT.POST_ADDED:
-        case AT.ADD_POST_ERROR:
+        case AT.ADD_POST_ERROR: {
+            return {...state, posts: false};
+        }
+        case AT.FETCH_USER:{
+            return {...state, users: true}
+        }
         case AT.RECEIVED_USER:
         case AT.FETCH_USER_ERROR: {
-            return false;
+            return {...state,users: false}
         }
         default:
             return state;
