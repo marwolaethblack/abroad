9aac0bbc05512aff89070bf88f4db74aef42ce35
Jakub Kozák <kozak.jakub55@gmail.com>
2017-06-01 14:44:08 +0200

    show whether a post is answered

--- client/src/post/Post.js
+++ client/src/post/Post.js
@@ -6,7 +6,7 @@ import { beautifyUrlSegment, spaceToDash } from '../services/textFormatting';
 
 class Post extends Component {
     render() {
-        const { upvotes,image,_id,title,author,comments,category, country_in } = this.props;
+        const { upvotes,image,_id,title,author,comments,category, country_in, isAnswered } = this.props;
         const datePosted = postDateDiff(_id);
 
         return(
@@ -14,12 +14,15 @@ class Post extends Component {
 	        	<span>Upvotes {upvotes}</span>
 	        	<img src={image ? image : `images/post-categories/${category}.jpg`} alt={title} />
 	        	<Link to={`/posts/${_id}/${spaceToDash(country_in)}/${category}/${beautifyUrlSegment(title)}`}><h3>{title}</h3></Link>
-	        	<span>
+	        	{ isAnswered && <span style={{color:'green',fontSize:'1.5em',fontWeight:'bold'}}>Answered <br /></span> }
+                <span>
                     Submitted {datePosted} ago by 
                     {author !== undefined && <Link to={`/user/${author._id}/${spaceToDash(author.username)}`}> {author.username} </Link>}
                     to {category}
                 </span>
-	        	<p>Comments: {comments !== undefined&& comments.length}</p>
+	        	<p> Comments: {comments !== undefined && comments.length} </p>
+               
+                
         	</article>
         );
     }
--- client/src/post/RelatedPost.js
+++ client/src/post/RelatedPost.js
@@ -6,13 +6,14 @@ import { beautifyUrlSegment, spaceToDash } from '../services/textFormatting';
 
 class RelatedPost extends Component {
     render() {
-        const { upvotes,_id,title,category, comments, country_from, country_in } = this.props;
+        const { upvotes,_id,title,category, comments, country_from, country_in, isAnswered } = this.props;
 
         return(
         	<div>        	
 	        	<Link to={`/posts/${_id}/${spaceToDash(country_in)}/${category}/${beautifyUrlSegment(title)}`}><h3>{title}</h3></Link>
                 <p>{country_from + " > " + country_in}</p>
 	        	<p>{"Category: " + category}</p>
+                { isAnswered && <span style={{color:'green',fontSize:'1.2em',fontWeight:'bold'}}>Answered <br /></span> }
                 <span>Upvotes {upvotes}</span> &nbsp;
                 <span>Comments {comments.length}</span>
         	</div>
